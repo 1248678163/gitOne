e92199ba3d317363306a5a06b4748e02a8c98511
卓商 <heanle59@163.com>
2018-06-11 18:13:09 +0800

    Initial Commit

---
+++ gitOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitOne
+//
+//  Created by 陛下 on 2018/6/11.
+//  Copyright © 2018年 卓商. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
